448b5776c1d4c151826adcaef9a63311f25742b5
Mike Bradford <mbradford@copperegg.com>
2014-02-16 23:28:39 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'desk', '~> 1.0.0'
+
+group :test do
+  gem 'rspec-rails', '~> 3.0.0.beta'
+  gem 'vcr', '~> 2.8.0'
+end
+
+gem 'rails_12factor', group: :production
+
+# Use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.1.2'
+
+ruby '2.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deskapi::Application.config.secret_key_base = '44bdae669080bf4e741553a8fde5ed4959689d8c956c72f3ab9e5c46f2f7bf83a5d56c0994cea2317a5cfe3e0d8fd21144523df3a29733cb56a5f143dcbadce0'
